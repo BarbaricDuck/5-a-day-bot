f003083e29f31667bf3923456f60ac1cbf26f88b
Ben Groves <ben_groves2004@outlook.com>
2021-04-11 19:24:20 +0100

    'added'

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ short_months = {'Dec':'December', 'Jan':'January', 'Feb':'February', 'Mar':'Marc
 
 
 
-s3 = S3Connection(os.environ['TOKEN'])
+s3 = S3Connection(os.environ['S3_KEY'], os.environ['S3_SECRET'])
 bot = commands.Bot(command_prefix='!')
 
 @bot.event
